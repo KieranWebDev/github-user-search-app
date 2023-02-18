543ebefe9bd63a02e4e565527e2068250714259e
Kieran <kieran.webdevelopment@gmail.com>
2023-02-18 16:10:52 +0000

    header complete

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
+
+import lightmode from '../images/icon-sun.svg';
+import darkmode from '../images/icon-moon.svg';
+
 import './NavBar.css';
 
 export default function NavBar() {
-  return <div>Navbar</div>;
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <header>
+      <h1>devfinder</h1>
+      {darkMode ? (
+        <button onClick={() => setDarkMode(!darkMode)}>
+          <span>LIGHT</span>
+          <img src={lightmode} alt="lightmode" />
+        </button>
+      ) : (
+        <button onClick={() => setDarkMode(!darkMode)}>
+          <span>DARK</span>
+          <img src={darkmode} alt="darkmode" />
+        </button>
+      )}
+    </header>
+  );
 }
